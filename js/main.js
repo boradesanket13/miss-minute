06a40bacb2f1e9458647d9838c635460793ac1d6
zainbinfurqan <zain.ahmed199524@gmail.com>
2022-10-10 03:40:10 +0500

    update icons for theme change

--- js/main.js
+++ js/main.js
@@ -1,36 +1,40 @@
 var dateInfo = new Date();
 var hr = dateInfo.getHours() > 12 ? dateInfo.getHours() - 12 : dateInfo.getHours(),
-    min = dateInfo.getMinutes(),
-    sec = dateInfo.getSeconds(),
-    millisec = dateInfo.getMilliseconds();
+  min = dateInfo.getMinutes(),
+  sec = dateInfo.getSeconds(),
+  millisec = dateInfo.getMilliseconds();
 
 var hrAngle = hr * 30 + (min * 6 / 12),
-    minAngle = min * 6 + (sec * 6 / 60),
-    secAngle = sec * 6 + (millisec * 0.36 / 1000);
+  minAngle = min * 6 + (sec * 6 / 60),
+  secAngle = sec * 6 + (millisec * 0.36 / 1000);
 
 // set initial angles of the hand wrappers
 function setAngle(wrapper, angle) {
-    document.querySelector("." + wrapper).style.transform = "rotate(" + angle + "deg)";
+  document.querySelector("." + wrapper).style.transform = "rotate(" + angle + "deg)";
 }
 setAngle("hr-hand", hrAngle);
 setAngle("min-hand", minAngle);
 setAngle("sec-hand", secAngle);
 
 function themeChange() {
-    let element = document.body;
-    element.classList.toggle("darkmodecss");
-    element.classList.toggle("cite");
-    var x = document.querySelector(".heading")
-    
-    if (x.innerHTML === "Light") {
-        x.innerHTML = "Dark";
-        document.getElementById("icon").classList.add('bxs-moon');
-        document.getElementById("icon").classList.remove('bxs-sun');
-    } else {
-        x.innerHTML = "Light";
-        document.getElementById("icon").classList.add('bxs-sun');
-        document.getElementById("icon").classList.remove('bxs-moon');
-    }
+  let element = document.body;
+  element.classList.toggle("darkmodecss");
+  element.classList.toggle("cite");
+  var x = document.querySelector(".heading")
+
+  if (x.innerHTML === "Light") {
+    x.innerHTML = "Dark";
+    document.getElementById("icon").classList.add('bxs-moon');
+    document.getElementById("icon").classList.remove('bxs-sun');
+    document.getElementById('linkedIn').style.fill = 'white'
+    document.getElementById('github').style.fill = 'white'
+  } else {
+    x.innerHTML = "Light";
+    document.getElementById("icon").classList.add('bxs-sun');
+    document.getElementById("icon").classList.remove('bxs-moon');
+    document.getElementById('linkedIn').style.fill = 'black'
+    document.getElementById('github').style.fill = 'black'
+  }
 }
 
 // quotes
